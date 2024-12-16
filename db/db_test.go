81a834df1ea5e0d8e61b2f2e5aaf10519fbe5dcd
LethalEthan <ethanjhem.e@gmail.com>
2024-12-16 15:11:43 +0000

    Add tests

--- db/db_test.go
+++ db/db_test.go
@@ -1,6 +1,7 @@
 package db
 
 import (
+	"context"
 	"testing"
 
 	kivik "github.com/go-kivik/kivik/v4"
@@ -16,5 +17,39 @@ func TestDBConnect(t *testing.T) {
 }
 
 func TestDBCreateDoc(t *testing.T) {
+	client, err := kivik.New("couch", "http://admin:Dexter233@localhost:5984/")
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	db := client.DB("test")
+	if db.Err() != nil {
+		t.Log("If no test db is pre existing ignore error and run tests again")
+		t.Error(err)
+		client.CreateDB(context.TODO(), "test")
+	}
+	doc := map[string]interface{}{
+		"test":  "test",
+		"test2": "test2",
+		"test3": 200,
+	}
+	id, rev, err := db.CreateDoc(context.TODO(), doc)
+	if err != nil {
+		t.Error(err)
+	}
+	t.Log(id, rev)
+}
 
+func TestCDBRegisterUser(t *testing.T) {
+	err := CDBRegisterUser("test@email.com", "test", "test", "test")
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestCDBGetUserByEmail(t *testing.T) {
+	_, err := CDBGetUserByEmail("test@email.com")
+	if err != nil {
+		t.Error(err)
+	}
 }
